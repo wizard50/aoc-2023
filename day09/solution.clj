49fcb3ba53e9a56df20b4f59afc579dc587df779
wizard50 <wizard50@tuta.io>
2023-12-10 17:10:06 -0300

    solution day09 part2

--- day09/solution.clj
+++ day09/solution.clj
@@ -13,31 +13,38 @@
 (defn extrapolate [last-col-vals]
   (reduce + last-col-vals))
 
-(defn get-last-col-vals [seq & acc]
+(defn calc-diff-rows [seq & acc]
   (let [diff-seq (calc-diff-seq seq)
         last-vals (if acc
-                    (conj acc (last diff-seq))
-                    (conj [(last seq)] (last diff-seq)))]
-    (if (every? #(= (first seq) %) diff-seq)
+                    (conj acc diff-seq)
+                    (conj [seq diff-seq]))]
+    (if (every? zero? diff-seq)
       last-vals
       (recur diff-seq last-vals))))
 
-(defn solve [input]
-  (->> input
-       (parse)
-       (map get-last-col-vals)
+(defn solve [lines]
+  (->> lines
+       (map calc-diff-rows)
+       (map #(map last %))
        (map extrapolate)
        (reduce +)))
+
+(defn solve1 [input]
+  (solve (parse input)))
+
+(defn solve2 [input]
+  (solve (map reverse (parse input))))
 
 (defn main []
   (let [text (slurp "day09/sample-input.txt")
         input (slurp "day09/input.txt")]
 
     ; sample
-    (println "last col values - sample 1: " (map get-last-col-vals (parse text)))
-    (println "sum of extrapolated values - sample 1: " (solve text))
+    (println "sum of extrapolated values - sample 1: " (solve1 text))
+    (println "sum of extrapolated values - sample 2: " (solve2 text))
 
     ; solution
-    (println "sum of extrapolated values - part1: " (solve input))
+    (println "sum of extrapolated values - part1: " (solve1 input))
+    (println "sum of extrapolated values - part2: " (solve2 input))
     ))
 
