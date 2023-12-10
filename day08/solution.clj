c3d06076b9ba81979a18627cd0183c96eee906bd
wizard50 <wizard50@tuta.io>
2023-12-10 11:05:17 -0300

    solution day08 part2

--- day08/solution.clj
+++ day08/solution.clj
@@ -1,32 +1,52 @@
 (ns day08.solution
   (:require [clojure.string :as str]))
 
+(defn gcd
+  ([x y]
+   (cond (zero? x) y
+         (< y x)   (recur y x)
+         :else     (recur x (rem y x)))))
+
+(defn lcm [x y]
+  (/ (* x y) (gcd x y)))
+
 (defn parse [input]
-  (let [[inst nodes] (str/split input #"\n\n")
-        node-lines (str/split-lines nodes)]
+  (let [[inst nodes] (str/split input #"\n\n")]
     {:inst (str/trim inst)
      :nodes (reduce #(let [[name left right] (re-seq  #"\w+" %2)]
-                            (assoc %1 name {:name name :left left :right right}))
+                            (assoc %1 name {:left left :right right}))
                     {}
-                    node-lines)}))
+                    (str/split-lines nodes))}))
 
-(defn navigate [map]
-  (let [len (count (:inst map))]
+(defn get-node-keys [map ends-with]
+  (filter #(str/ends-with? % ends-with) (keys (:nodes map))))
+
+(defn navigate [map & [start-key end-fn]]
+  (let [len (count (:inst map))
+        start-key (if start-key start-key "AAA")
+        end-fn (if end-fn end-fn #(= % "ZZZ"))]
     (loop [i 0
-           node (get (:nodes map) "AAA")
+           node (get (:nodes map) start-key)
            finish? false]
       (if (not finish?)
         (let [inst (nth (:inst map) (mod i len))
-              next-name (get node (case inst
+              next-key (get node (case inst
                                     \L :left
                                     \R :right))]
           (recur (inc i)
-                 (get (:nodes map) next-name)
-                 (= next-name "ZZZ")))
+                 (get (:nodes map) next-key)
+                 (end-fn next-key)))
         i))))
 
 (defn solve [input]
   (navigate (parse input)))
+
+(defn solve2 [input]
+  (let [data (parse input)
+        steps (map (fn [k]
+                    (navigate data k #(str/ends-with? % "Z")))
+                  (get-node-keys data "A"))]
+  (reduce lcm steps)))
 
 (defn main []
   (let [text (slurp "day08/sample-input.txt")
@@ -37,7 +57,9 @@
     (println "navigation steps needed (RL) - sample 1:" (solve text))
     (println "navigation steps needed (LLR) - sample 1:" (solve text2))
 
-    ; solution
-    (println "navigation steps needed - part 1:" (solve input))
-    ))
+    (println "starting node keys" (get-node-keys (parse input) "A"))
+    (println "ending node keys" (get-node-keys (parse input) "Z"))
 
+    ;; solution
+    (println "navigation steps needed - part 1:" (solve input))
+    (println "navigation steps needed - part 2:" (solve2 input))))
